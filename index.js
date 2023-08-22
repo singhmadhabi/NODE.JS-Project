a34f7174885b69f5021def996e9ff08fd22bc39d
Madhabi Singh <singhmadhabi4@gmail.com>
2023-08-22 09:14:36 +0545

    learn about npm

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const http = require("http"); //nodejs inbuilt package
 const myModules = require("./myCustomModule");
-
+const upperCase = require("upper-case");
 http
   .createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/html" }); //html tag
     const date = myModules.myDate();
     const myTime = myModules.myTime();
-    res.write("<h1>Hello World</h1><p>" + date + "</p><p>" + myTime + "</p>"); //body
+    const myName = upperCase.upperCase("Madhabi");
+    res.write(
+      "<h1>Hello World," +
+        myName +
+        "</h1><p>" +
+        date +
+        "</p><p>" +
+        myTime +
+        "</p>"
+    ); //body
     res.end();
   })
   .listen(5555);
